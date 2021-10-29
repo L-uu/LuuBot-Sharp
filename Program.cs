a7c70c3a80cab1bb3b24c768e1b988665393e136
L-uu <louie.nutman@gmail.com>
2021-10-29 18:11:04 +0100

    start with basic bot

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using DSharpPlus;
+using Newtonsoft.Json;
+
+namespace LuuBot_Sharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MainAsync().GetAwaiter().GetResult();
+		}
+
+		static async Task MainAsync()
+		{
+			string token = File.ReadAllText("token.txt");
+			var discord = new DiscordClient(new DiscordConfiguration()
+			{
+				Token = token,
+				TokenType = TokenType.Bot,
+				Intents = DiscordIntents.AllUnprivileged
+			});
+			await discord.ConnectAsync();
+			await Task.Delay(-1);
+		}
+	}
+}
